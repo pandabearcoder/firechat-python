d3810644957e1483afec024e9d67fde7781bddd3
pandabearcoder <vlouie00@gmail.com>
2018-03-06 20:22:19 +0800

    Modified to a python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firechat',
+    version='0.1',
+    description='Port of the javascript firechat sdk to python with the help of firebase-admin',
+    url='https://github.com/pandabearcoder/firechat-python',
+    author='pandabearcoder',
+    author_email='vlouie00@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+)
