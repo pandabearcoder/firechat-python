aa100f6c200bf38bc080537e643eba2725ec4d31
pandabearcoder <vlouie00@gmail.com>
2018-03-06 20:26:27 +0800

    Updated package metadata

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='firechat',
     version='0.1',
     description='Port of the javascript firechat sdk to python with the help of firebase-admin',
+    long_description=readme,
     url='https://github.com/pandabearcoder/firechat-python',
     author='pandabearcoder',
     author_email='vlouie00@gmail.com',
     license='MIT',
-    packages=find_packages(),
+    keywords='firechat firebase chat',
+    packages=['firechat'],
     zip_safe=False,
 )
